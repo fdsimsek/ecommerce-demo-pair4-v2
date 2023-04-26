ba45974e0f75e875d5f1f8b19f4d5f408ecb51eb
fdsimsek <firat-deniz143@hotmail.com>
2023-04-26 14:26:25 +0300

    get by id, update, delete

--- src/main/java/com/etiya/ecommercedemopair4/business/requests/UpdateCustomerRequest.java
+++ src/main/java/com/etiya/ecommercedemopair4/business/requests/UpdateCustomerRequest.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateCustomerRequest {
+    private int id;
     private String companyName;
     private String contactName;
     private String emailAddress;
