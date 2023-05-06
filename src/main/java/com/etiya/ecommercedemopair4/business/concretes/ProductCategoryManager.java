b4bf721b1a6bae37c30308640f1cdbfc3db80cee
firatsimsek <firat.simsek@etiya.com>
2023-05-06 13:47:37 +0300

    last version

--- src/main/java/com/etiya/ecommercedemopair4/business/concretes/ProductCategoryManager.java
+++ src/main/java/com/etiya/ecommercedemopair4/business/concretes/ProductCategoryManager.java
@@ -5,13 +5,9 @@ import com.etiya.ecommercedemopair4.business.dtos.requests.productCategory.AddPr
 import com.etiya.ecommercedemopair4.core.utilities.mappers.ModelMapperService;
 import com.etiya.ecommercedemopair4.core.utilities.result.Result;
 import com.etiya.ecommercedemopair4.core.utilities.result.SuccessResult;
-import com.etiya.ecommercedemopair4.entities.concretes.Category;
-import com.etiya.ecommercedemopair4.entities.concretes.Product;
 import com.etiya.ecommercedemopair4.entities.concretes.ProductCategory;
 import com.etiya.ecommercedemopair4.repositories.abstracts.ProductCategoryDao;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,7 +17,6 @@ import java.util.List;
 public class ProductCategoryManager implements ProductCategoryService {
     private ProductCategoryDao productCategoryDao;
     private ModelMapperService modelMapperService;
-    private MessageSource messageSource;
     @Override
     public Result addRange(int productId, List<AddProductCategoryRequest> addProductCategoryRequests) {
         for (AddProductCategoryRequest request : addProductCategoryRequests) {
